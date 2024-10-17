e9a33275bc0e22013d1d784f26736ce3dd9a5c51
ag-015 <aditya.galgate@texple.com>
2024-10-17 15:07:28 +0530

    changes the common font style of all mui components

---
+++ src/components/Fonts/FontDetails.js
@@ -0,0 +1,14 @@
+const FontDetails = {
+  fontStyle: 'Calibri',
+  typographySize: '12px'
+  //   paginationTotalCount: '12px',
+  //   marginPaginationSelectIcon: '-3px',
+  //   paginationSelectIcon: '8px',
+  //   datagridColumnHeaderFontSize: '12px',
+  //   CheckBoxSize: '10',
+  //   rowsCountSelectDatagrid: '12px',
+  //   rowsPerPage: '12px',
+  //   marginTopRowsPerPage: '2'
+}
+
+export default FontDetails
