ba9cad666b5585585875120632704cea10cd29d9
unknown <gouse.shaikh@texple.com>
2024-10-28 13:35:51 +0530

    domain url

--- src/configs/awsConfig.js
+++ src/configs/awsConfig.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://175.184.255.158:5555/api/v1'
-// const BASE_URL = 'https://oneplace.nirmalbang.com/api/v1'
+// const BASE_URL = 'http://175.184.255.158:5555/api/v1'
+const BASE_URL = 'https://oneplace.nirmalbang.com/api/v1'
 
 export default {
   BASE_URL: BASE_URL
